9261e74ed68d29b634d9f297c58f5dc256e40ba4
Armin Biere <biere@cs.uni-freiburg.de>
2023-03-06 05:21:10 +0100

    ported to python 3

--- scripts/bbcmp.py
+++ scripts/bbcmp.py
@@ -6,12 +6,12 @@ from bb import *
 asz=len(sys.argv)-1
 
 if ((asz&1)==1): 
-  print 'even no. of args expected'
+  print ('even no. of args expected')
 
 args=sys.argv[1:]
 
 bss=[]
-for i in range(asz/2):
+for i in range(int(asz/2)):
     f = open(args[i*2+1], 'r')
     bs=rbb(args[i*2], f)
     bss.append(bs)
@@ -19,11 +19,11 @@ for i in range(asz/2):
 wrong=False
 for i in range(len(bss)-1):
   for j in range(i+1,len(bss)):
-    print 'comparing',args[i*2+1],args[j*2+1]
+    print ('comparing',args[i*2+1],args[j*2+1])
     if bss[i]!=bss[j]:
-      print 'differ: ',args[i*2+1],args[j*2+1]
-      print bss[i]-bss[j]
-      print bss[j]-bss[i]
+      print ('differ: ',args[i*2+1],args[j*2+1])
+      print (bss[i]-bss[j])
+      print (bss[j]-bss[i])
       wrong=True
 
 exit(100 if wrong else 0)
